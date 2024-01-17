3514262ae920a2d09710f4644187c26b80c3b4ab
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2024-01-17 16:16:37 -0500

    wrote pseudocode

--- home.py
+++ home.py
@@ -1,1 +1,24 @@
 import streamlit as st
+
+'whats up'
+
+'''
+1. Create an array of voters, social media posts, and votes
+    - Find active Farcaster users that have been active on Snapshot ()
+2.
+
+
+For all the spaces on Snapshot,
+Review all the proposals in each Space,
+Collect the Ethereum addresses of each of the voters,
+
+For all active Farcaster users,
+Collect the active Farcaster user eth addresses,
+Compare the voters addresses to active Farcaster eth addresses,
+Collect a list of Farcaster users with 
+
+Collect social media posts of Farcaster and Snapshot users,
+
+Return a list of Farcaster eth addresses, the social media posts they made, and the votes they made 
+
+'''
